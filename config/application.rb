a741f072dc39ab804b0baffe8361d9e38d7b5ff2
salsasource <salsasource31@gmail.com>
2022-06-17 03:33:58 +0000

    [Add] ジェネレータの設定

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,13 @@ module Baukis2
     config.i18n.load_path +=
       Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
