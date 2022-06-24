30ab84c2ac743fcf7e6bf4e9e6df6abe569fc6f2
salsasource <salsasource31@gmail.com>
2022-06-24 21:42:27 +0900

    staff_events/index.html.erbを作成

--- app/models/staff_event.rb
+++ app/models/staff_event.rb
@@ -2,5 +2,15 @@ class StaffEvent < ApplicationRecord
   self.inheritance_column = nil
 
   belongs_to :member, class_name: "StaffMember", foreign_key: "staff_member_id"
-  alias.attributes :occurred_at, :created_at
+  alias_attribute :occurred_at, :created_at
+
+  DESCRIPTIONS = {
+    logged_in: "ログイン",
+    logged_out: "ログアウト",
+    rejected: "ログイン拒否"
+  }
+
+  def description
+    DESCRIPTIONS[type.to_sym]
+  end
 end
