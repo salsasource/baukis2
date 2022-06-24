bbd658dbe61a88d5bf760cb26a5ee91dcac3c5df
salsasource <salsasource31@gmail.com>
2022-06-24 17:14:27 +0900

    管理者ページに強制ログアウトの仕組みを追加

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,6 @@
 class Admin::Base < ApplicationController
   before_action :authorize
+  before_action :check_account
 
   private def current_administrator
     if session[:administrator_id]
@@ -9,11 +10,21 @@ class Admin::Base < ApplicationController
   end
 
   helper_method :current_administrator
-
+  
+  # アクセス制御
   private def authorize
     unless current_administrator
       flash.alert = "管理者としてログインしてください。"
       redirect_to :admin_login
+    end
+  end
+
+  # 強制ログアウト
+  private def check_account
+    if current_administrator && !current_administrator.active?
+      session.delete(:staff_member_id)
+      flash.alert = "アカウントが無効になりました。"
+      redirect_to :admin_root
     end
   end
 end
--- spec/requests/admin/staff_members_spec.rb
+++ spec/requests/admin/staff_members_spec.rb
@@ -19,6 +19,21 @@ describe "管理者による職員管理" do
       }
   end
 
+  describe "情報表示" do
+    let(:staff_member) { create(:staff_member) }
+    
+    example "成功" do
+      get admin_staff_members_url
+      expect(response.status).to eq(200)
+    end
+
+    example "停止フラグがセットされたら強制的にログアウト" do
+      administrator.update_column(:suspended, true)
+      get admin_staff_members_url
+      expect(response).to redirect_to(admin_root_url)
+    end
+  end
+
   describe "新規登録" do
     let(:params_hash) { attributes_for(:staff_member) }
 
