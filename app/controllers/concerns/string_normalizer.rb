39e2a9450164f10d44fa1aa1281c48124f1afa8a
salsasource <salsasource31@gmail.com>
2022-06-25 18:28:12 +0900

    メールアドレスの正規化とバリデーションを追加

--- app/controllers/concerns/string_normalizer.rb
+++ app/controllers/concerns/string_normalizer.rb
@@ -10,4 +10,8 @@ module StringNormalizer
   def normalize_as_furigana(text)
     NKF.nkf("-W -w -Z1 --katakana", text).strip if text
   end
+
+  def normalize_as_email(text)
+    NKF.nkf("-W -w -Z1", text).strip if text
+  end
 end
