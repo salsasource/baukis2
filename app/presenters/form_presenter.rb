416a113f74349248f310d31ebba5ecd4a0510beb
salsasource <salsasource31@gmail.com>
2022-06-26 17:24:30 +0900

    入力エラーメッセージの生成

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -21,6 +21,7 @@ class FormPresenter
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << text_field(name, options)
+      m << error_messages_for(name)
     end
   end
 
@@ -28,13 +29,25 @@ class FormPresenter
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << text_field(name, options)
+      m << error_messages_for(name)
     end
   end
 
   def date_field_block(name, label_text, options = {})
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
-      m << text_field(name, options)
+      m << date_field(name, options)
+      m << error_messages_for(name)
+    end
+  end
+
+  def error_messages_for(name)
+    markup do |m|
+      object.errors.full_messages_for(name).each do |message|
+        m.div(class: "error-message") do |m|
+          m.text message
+        end
+      end
     end
   end
 
