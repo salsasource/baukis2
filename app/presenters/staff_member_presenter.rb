682cbc5f7565809f7e7b6d6cfc29e4fe5c795388
salsasource <salsasource31@gmail.com>
2022-06-26 17:49:48 +0900

    氏名表示を修正

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -7,4 +7,16 @@ class StaffMemberPresenter < ModelPresenter
   def suspended_mark
     suspended? ? raw("&#x2611;") : raw("&#x2610;")
   end
+
+  def full_name
+    markup do |m|
+      m.td "#{object.family_name} #{object.given_name}"
+    end
+  end
+
+  def full_name_kana
+    markup do |m|
+      m.td "#{object.family_name_kana} #{object.given_name_kana}"
+    end
+  end
 end
